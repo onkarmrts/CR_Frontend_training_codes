1295a7fe8261c5a2bfe9cbe73accc1af068f6d73
unknown <mathapationkar8@gmail.com>
2022-10-15 20:43:01 +0530

    js for react

---
+++ calc.js
@@ -0,0 +1,16 @@
+// const sum = (a, b) => {
+//           return a + b;
+// }
+
+// const sub = (a, b) => {
+//           return a-b;
+// }
+
+// export {sum, sub};
+
+
+// const sayMyName = () => {
+//           return 'collegeranker'
+// }
+
+// export default sayMyName;
---
+++ index.js
@@ -0,0 +1,6 @@
+import sayMyName from './calc.js'
+
+// console.log('the sum is', sum(2,3))
+// console.log('the sub is', sub(4,3))
+
+console.log(sayMyName())
